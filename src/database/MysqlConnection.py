5894208e731d3181323fa465d4fd1083a614ea3f
Luska <lrv.luccas123@gmail.com>
2022-07-25 01:17:07 -0300

    Criando pastas do projeto e conexão com banco de dados

---
+++ src/database/MysqlConnection.py
@@ -0,0 +1,17 @@
+from warnings import catch_warnings
+import mysql.connector
+from mysql.connector import errorcode
+
+dbconfig = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '123456',
+}
+
+try:
+   conn = mysql.connector.connect(**dbconfig)
+   print("Conectado com sucesso")
+   mycursor = conn.cursor();
+except mysql.connector.Error as err:
+    print("Falha ao conectar")
+
