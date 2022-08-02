923cbd7bae662ace0b087ae2dc426a4da4343fd1
Arthur-Carlos <93096866+Arthur-Carlos@users.noreply.github.com>
2022-08-02 10:34:02 -0300

    Adicionada a view

--- src/database/TrabalhoBd.sql
+++ src/database/TrabalhoBd.sql
@@ -20,6 +20,7 @@ Create Table Funcionario(
     nome_fun varchar(30) not null,
     telefone_fun varchar(11),
     endereco_fun varchar(30),
+    disp_fun int,
     cod_emp int not null,
     equip_fun varchar(30),
     constraint foreign key (cod_emp) references Empresa(cod_emp)
@@ -29,6 +30,7 @@ Create Table Extra(
     cod_ext int not null primary key,
     nome_ext varchar(30),
     preco_ext int not null,
+    disp_ext int,
     hora_ext int,
     tipo_ext varchar(30)
 );
@@ -39,6 +41,7 @@ Create Table Festa(
     tipo_fes varchar(30) not null,
     nome_fes varchar(30) not null,
     duracao_fes int,
+    status_fes varchar(5),
     cod_cli int not null,
     cod_emp int not null,
     constraint foreign key (cod_emp) references Empresa(cod_emp),
@@ -52,3 +55,9 @@ Create Table Servico(
     constraint foreign key (cod_fes) references Festa(cod_fes),
     constraint foreign key (cod_ext) references Extra(cod_ext)
 );
+create view Conta as
+select f.nome_fes as "Nome Da Festa", c.nome_cli as "Comprador" , sum(preco_ext) + preco_fes as Total
+from Festa f inner join Cliente c on f.cod_cli= c.cod_cli
+inner join Servico s on s.cod_fes = f.cod_fes
+inner join Extra e on e.cod_ext = s.cod_ext
+group by f.nome_fes, c.nome_cli;
